ae626fd32c824619daf6e1f33ec1dd7b62758e79
Caio Tony <caiotony16@gmail.com>
2022-03-14 17:36:03 +0100

    feat: add war entity

---
+++ src/domain/entities/war.ts
@@ -0,0 +1,34 @@
+import { EntityValidationError } from './errors';
+import { Entity, EntityValidator } from './interfaces';
+
+export class War implements Entity<war_type> {
+
+	constructor(
+		private readonly entity: war_type,
+        private readonly validator: EntityValidator<war_type>
+	) {}
+
+	async validate(): Promise<EntityValidationError | null> {
+		if (this.entity.begin.year > (new Date()).getFullYear()) {
+			return new EntityValidationError();
+		}
+		return this.validator.validate(this.entity) ;		
+	}
+	
+	public get getEntity() : war_type {
+		return this.entity;
+	}
+	
+}
+
+
+export type war_type = {
+    name: string
+    begin: {
+		day: number,
+		month: number,
+		year: number
+	}
+}
+
+
